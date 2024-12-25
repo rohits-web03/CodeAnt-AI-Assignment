dc6943d1d1fe475d7ec466c1f1168b6364f1e352
rohits-web03 <rohit.dey302003@gmail.com>
2024-12-25 06:46:24 +0530

    Add routes / and /signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Signin from "./pages/Signin"
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import RepositoryScreen from './pages/RepositoryScreen';
+import SignIn from './pages/Signin';
 
-function App() {
-  return (
-    <Signin />
-  )
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RepositoryScreen />,
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App
